d1c488cf1828e27833836c614351bb9dc8b52eda
PoonamBhosale <pinubhosale5@gmail.com>
2017-06-27 17:17:11 +0530

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,20 @@
+  $scope.user = {
+                        nvdata: 'NV Data Management'
+                    };
+                    // $scope.editorOptions = {
+                    //     language: 'ru',
+                    //     uiColor: '#000000'
+                    // };
+                    // $scope.htmlEditor = 'Poonam Bhosale is Clever girl';
+                    // $scope.content = "<p> this is custom directive </p>";
+
+                    $scope.options = {
+                        language: 'en',
+                        allowedContent: true,
+                        entities: false
+                    };
+
+                    // Called when the editor is completely ready.
+                    $scope.onReady = function() {
+                        // ...
+                    };
